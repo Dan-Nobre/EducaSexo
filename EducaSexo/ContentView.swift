685b2e3d06ac3ee64eea61581d222a4f1b654f60
Dan-Nobre <danielaraujonobre14@gmail.com>
2024-05-07 10:43:09 -0300

    first commit

---
+++ EducaSexo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EducaSexo
+//
+//  Created by Daniel Nobre on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
